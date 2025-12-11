3e565c9fed3311db4b34e7901f5eaf1526b12bc0
bassanthamdy <bassanthamdyhasebelnabyabdelall.ahmed@studenti.unipd.it>
2025-12-11 18:55:10 +0100

    Update drone_sim.py

--- drone_sim.py
+++ drone_sim.py
@@ -1,58 +1,24 @@
-import numpy as np
+# drone_sim.py
 
+class Drone:
+    """A simplified 1D vertical dynamics model (altitude) with a PID controller."""
 
-class SimpleDrone:
-"""A simplified 1D vertical dynamics model (altitude) with a PID controller.
-State: z (altitude), v (vertical velocity)
-Control: thrust force (mapped to acceleration)
-"""
-def __init__(self, mass=1.0, dt=0.01):
-self.m = mass
-self.g = 9.81
-self.dt = dt
-self.reset()
+    def __init__(self):
+        self.z = 0.0   # altitude
+        self.v = 0.0   # vertical velocity
+        self.m = 1.0   # mass
+        self.g = 9.81  # gravity
+        self.dt = 0.01
 
+    def reset(self):
+        """Reset drone state"""
+        self.z = 0.0
+        self.v = 0.0
 
-def reset(self):
-self.z = 0.0
-self.v = 0.0
-self.time = 0.0
-
-
-def dynamics_step(self, thrust):
-# thrust is a force in N upward
-a = (thrust - self.m * self.g) / self.m
-# simple Euler integration
-self.v += a * self.dt
-self.z += self.v * self.dt
-self.time += self.dt
-return self.z, self.v
-
-
-
-
-class PIDController:
-def __init__(self, kp=12.0, ki=1.0, kd=3.0, dt=0.01, integrator_limit=10.0):
-self.kp = kp
-self.ki = ki
-self.kd = kd
-self.dt = dt
-self.integrator = 0.0
-self.prev_error = 0.0
-self.integrator_limit = integrator_limit
-
-
-def reset(self):
-self.integrator = 0.0
-self.prev_error = 0.0
-
-
-def compute(self, setpoint, measurement):
-error = setpoint - measurement
-self.integrator += error * self.dt
-# anti-windup
-self.integrator = max(min(self.integrator, self.integrator_limit), -self.integrator_limit)
-derivative = (error - self.prev_error) / self.dt
-self.prev_error = error
-u = self.kp * error + self.ki * self.integrator + self.kd * derivative
-return u
+    def dynamics_step(self, thrust):
+        """Compute next state given thrust"""
+        # simple vertical dynamics: a = (thrust - mg)/m
+        a = (thrust - self.m * self.g) / self.m
+        self.v += a * self.dt
+        self.z += self.v * self.dt
+        return self.z, self.v
